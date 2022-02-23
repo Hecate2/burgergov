0aab8646e3b95da0f18d0b88643ef973502b9c94
Hecate2 <2474101468@qq.com>
2022-02-23 14:51:49 +0800

    /debug/crash

--- http.debug.go
+++ http.debug.go
@@ -8,4 +8,7 @@ func init() {
 	http.HandleFunc("/debug/refresh", func(w http.ResponseWriter, r *http.Request) {
 		go data.biz_refresh()
 	})
+	http.HandleFunc("/debug/crash", func(w http.ResponseWriter, r *http.Request) {
+		log.Fatalln("This is to crash the app on purpose")
+	})
 }
