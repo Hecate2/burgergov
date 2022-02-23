94b3d785ed6aff4842600566ed09eb843a7018b6
Hecate2 <2474101468@qq.com>
2022-02-23 14:59:27 +0800

    rollback to normal

--- http.debug.go
+++ http.debug.go
@@ -8,7 +8,4 @@ func init() {
 	http.HandleFunc("/debug/refresh", func(w http.ResponseWriter, r *http.Request) {
 		go data.biz_refresh()
 	})
-	http.HandleFunc("/debug/crash", func(w http.ResponseWriter, r *http.Request) {
-		panic("This is to crash the app on purpose")
-	})
 }
