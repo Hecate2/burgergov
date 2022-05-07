6519b53cec3237971f5d03928ccdd44133830a49
Hecate2 <2474101468@qq.com>
2022-05-07 21:04:39 +0800

    fix header

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ func main() {
 
 func setCorsHeaders(innerFunc func(http.ResponseWriter, *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		innerFunc(w, r)
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		innerFunc(w, r)
 	}
 }
